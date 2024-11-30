f9a97760aff6a8cca042fc81af22d3867581f0b8
churchillogenrwot <churchillogenrwot7@gmail.com>
2024-11-30 10:21:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 //const session = require('express-session'); 
+const cors = require('cors');
+
+app.use(cors());  // Allow all origins (for development purposes)
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,6 +20,12 @@ app.use('/budgets', require('./routes/budget'));
 app.use('/reports', require('./routes/reports'));
 app.use('/users', require('./routes/user'));
 
+const corsOptions = {
+    origin: 'http://localhost:3000',  // Your frontend URL
+  };
+  
+  app.use(cors(corsOptions));
+  
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
